41bc9bd09e6e64cba07b809f44d1aad94fdbb034
BigRedEye <skvor.sm@gmail.com>
2017-11-25 18:53:00 +0300

    Remove debug log

--- include/sdl_ptr.h
+++ include/sdl_ptr.h
@@ -3,7 +3,6 @@
 
 #include <memory>
 #include <SDL2/SDL.h>
-#include <iostream>
 
 namespace term {
 
@@ -12,7 +11,6 @@ struct sdl_deleter {
     void operator()(T* _ptr) {
         std::default_delete<T> del;
         del(_ptr);
-        std::cerr << "Default deleter called" << std::endl;
     }
 };
 
@@ -20,7 +18,6 @@ template< >
 struct sdl_deleter<SDL_Window> {
     void operator()(SDL_Window* _ptr) {
         SDL_DestroyWindow(_ptr);
-        std::cerr << "SDL_DestroyWindow() called" << std::endl;
     }
 };
 
@@ -28,7 +25,6 @@ template<>
 struct sdl_deleter<SDL_Renderer> {
     void operator()(SDL_Renderer* _ptr) {
         SDL_DestroyRenderer(_ptr);
-        std::cerr << "SDL_DestroyRenderer() called" << std::endl;
     }
 };
 
@@ -36,7 +32,6 @@ template<>
 struct sdl_deleter<SDL_Surface> {
     void operator()(SDL_Surface* _ptr) {
         SDL_FreeSurface(_ptr);
-        std::cerr << "SDL_FreeSurface() called" << std::endl;
     }
 };
 
@@ -44,7 +39,6 @@ template<>
 struct sdl_deleter<SDL_Texture> {
     void operator()(SDL_Texture* _ptr) {
         SDL_DestroyTexture(_ptr);
-        std::cerr << "SDL_DestroyTexture() called" << std::endl;
     }
 };
 
