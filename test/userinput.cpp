069e8834630a18a9210101bfc774ec31e837354a
BigRedEye <skvor.sm@gmail.com>
2018-04-13 02:52:05 +0300

    Rewrite userinput example

--- test/userinput.cpp
+++ test/userinput.cpp
@@ -1,6 +1,7 @@
 #include <rterm/term.h>
 #include <rterm/key.h>
 #include <rterm/mouse.h>
+#include <rterm/logger.h>
 
 #include <iostream>
 #include <random>
@@ -9,66 +10,29 @@
 
 #define UNUSED(cond) (void)(cond)
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
+    /* on Windows SDL2 redeclares main with custom main function,
+       so we need argc and argv */
     UNUSED(argc);
     UNUSED(argv);
-
+    /* creates empty terminal with 80 columns and 24 rows */
     rterm::Term terminal(80, 24);
+    terminal.setTitle("rterm demo");
+    /* loads TrueType font */
     int fontSize = 18;
-    terminal.setTitle("rterm demo")
-            .setIcon("terminal.ico");
     terminal.setFont("../fonts/ttf/DejaVuSansMono.ttf", fontSize);
-    terminal.setResizable(true);
-    terminal.resize(80, 24);
-
-    terminal.setFgColor(rterm::Color(0, 0xff, 0));
-
-    bool fullscr = false;
-    while (terminal.isRunning()) {
-        rterm::Key k = terminal.getKey(16);
-        if (k.key() == rterm::KeyCode::F4 && (k.mod() & rterm::KeyMod::ALT))
-            return 0;
-        switch (k.key()) {
-        case rterm::KeyCode::F1:
-            fullscr = !fullscr;
-            terminal.setFullscreen(fullscr);
-            break;
-        case rterm::KeyCode::UP:
-            terminal.shift(0, -1);
-            break;
-        case rterm::KeyCode::LEFT:
-            terminal.shift(-1, 0);
-            break;
-        case rterm::KeyCode::RIGHT:
-            terminal.shift(1, 0);
-            break;
-        case rterm::KeyCode::DOWN:
-            terminal.shift(0, 1);
-            break;
-        default:
-            break;
-        }
-        static int mx = -1,
-                   my = -1;
-        size_t x = 0,
-               y = 0;
-        terminal.getMousePosition(x, y);
-        if (terminal.getMouseButtons() & rterm::MouseButton::LEFT)
-            terminal.setBgColor(rterm::Color(0xff, 0xff, 0xff), x, y);
-        if (terminal.getMouseButtons() & rterm::MouseButton::RIGHT) {
-            if (mx >= 0 && my >= 0)
-                terminal.shift(x - mx, y - my);
-            mx = x;
-            my = y;
-        } else {
-            mx = -1;
-            my = -1;
-        }
-
-        if (k.toChar())
-            terminal.addChar(k.toChar());
+    /* sets foreground color to green (#00FF00)*/
+    terminal.setFgColor(rterm::Color(0x00, 0xff, 0x00));
+    /* sets callback for keypress */
+    terminal.onKeyDown([&](rterm::events::KeyDownEvent ev) {
+        /* if key has visual representation...*/
+        if (ev.key().toChar())
+            /* add character to terminal */
+            terminal.addChar(ev.key().toChar());
+        /* and draw everything on screen */
         terminal.redraw();
-    }
+    });
+    /* while terminal is not closed... */
+    while (terminal.isRunning());
     return 0;
 }
