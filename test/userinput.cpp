9889be20356229f3181eda6eb0fbaf799d6cc82c
BigRedEye <skvor.sm@gmail.com>
2018-08-12 23:00:27 +0300

    Error checking in examples

--- test/userinput.cpp
+++ test/userinput.cpp
@@ -1,39 +1,44 @@
-#include <rterm/term.h>
 #include <rterm/key.h>
-#include <rterm/mouse.h>
 #include <rterm/logger.h>
+#include <rterm/mouse.h>
+#include <rterm/term.h>
 
 #define UNUSED(cond) (void)(cond)
 
 int main(int argc, char **argv) {
     /* on Windows SDL2 redefines main with custom SDL_main function,
-       so we need argc and argv */
+     * so we need argc and argv 
+     */
     UNUSED(argc);
     UNUSED(argv);
-    /* create empty terminal with 80 columns and 24 rows */
-    rterm::Term terminal(80, 24);
-    /* set window title */
-    terminal.setTitle("rterm example");
-    /* load TrueType font */
-    int fontSize = 18;
-    terminal.setFont("../fonts/ttf/DejaVuSansMono.ttf", fontSize);
-    /* set foreground color to green (#00FF00) */
-    terminal.setFgColor(rterm::Color(0x00, 0xff, 0x00));
-    /* set callback for keypress */
-    terminal.onKeyDown([&](rterm::events::KeyDownEvent ev) {
-        /* if key has visual representation... */
-        if (ev.key().toChar()) {
-            /* add character to terminal */
-            terminal.addChar(ev.key().toChar());
-            /* and draw everything on screen */
-            terminal.redraw();
+    /* rterm checks every SDL2 call, so: */
+    try {
+        /* create empty terminal with 80 columns and 24 rows */
+        rterm::Term terminal(80, 24);
+        /* set window title */
+        terminal.setTitle("rterm example");
+        /* load TrueType font */
+        int fontSize = 18;
+        terminal.setFont("../fonts/ttf/DejaVuSansMono.ttf", fontSize);
+        /* set foreground color to green (#00FF00) */
+        terminal.setFgColor(rterm::Color(0x00, 0xff, 0x00));
+        /* set callback for keypress */
+        terminal.onKeyDown([&](rterm::events::KeyDownEvent ev) {
+            /* if key has visual representation... */
+            if (ev.key().toChar()) {
+                /* add character to terminal */
+                terminal.addChar(ev.key().toChar());
+                /* and draw everything on screen */
+                terminal.redraw();
+            }
+        });
+        /* while terminal is not closed... */
+        while (terminal.isRunning()) {
+            /* poll events */
+            terminal.poll();
         }
-    });
-    /* while terminal is not closed... */
-    while (terminal.isRunning()) {
-        /* poll events */
-        terminal.poll();
-        terminal.redraw();
+        return 0;
+    } catch (const rterm::Exception& e) {
+        rterm::Logger() << e.what();
     }
-    return 0;
 }
