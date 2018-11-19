899f12e751e58637e9249f95e55f264d9a89945d
BigRedEye <skvor.sm@gmail.com>
2018-11-19 18:57:38 +0300

    Better CI scripts

--- ci/install.bat
+++
@@ -1,8 +0,0 @@
-cd ..
-set MINGW=C:\mingw-w64\x86_64-7.2.0-posix-seh-rt_v5-rev\mingw64
-set INSTALL_PATH=%MINGW%\x86_64-w64-mingw32\
-copy librterm.dll %INSTALL_PATH%\bin\
-copy librterm.dll.a %INSTALL_PATH%\lib\
-mkdir %INSTALL_PATH%\include\rterm
-copy include %INSTALL_PATH%\include\rterm\
-cd ci
