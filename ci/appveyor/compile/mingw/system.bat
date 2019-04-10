cd75f50b185f1ef7808461495068c1e9821411bd
BigRedEye <mail@bigredeye.me>
2019-04-11 01:48:39 +0300

    ci(appveyor): Bump

--- ci/appveyor/compile/mingw/system.bat
+++ ci/appveyor/compile/mingw/system.bat
@@ -1,7 +1,7 @@
 pushd %cd% &&^
 mkdir build &&^
 cd build &&^
-cmake .. -DCMAKE_BUILD_TYPE=Release -DRTERM_EXTERNAL_LIBS=%RTERM_LIBS% -DRTERM_SDL2_PATH=%DEPS_PATH% -DRTERM_BUILD_TESTS=ON -G "MinGW Makefiles" &&^
+cmake .. -DCMAKE_BUILD_TYPE=Release -DRTERM_EXTERNAL_LIBS=%RTERM_LIBS% -DRTERM_SDL2_PATH=%DEPS_PATH% -DCMAKE_SH="CMAKE_SH-NOTFOUND" -DRTERM_BUILD_TESTS=ON -G "MinGW Makefiles" &&^
 cmake --build . --config Release &&^
 mkdir bin\Release &&^
 move bin\*.exe bin\Release &&^
--- ci/appveyor/compile/msvc/system.bat
+++ ci/appveyor/compile/msvc/system.bat
@@ -1,7 +1,7 @@
 pushd %cd% &&^
 mkdir build &&^
 cd build &&^
-cmake .. -DRTERM_BUILD_TESTS=ON -DRTERM_EXTERNAL_LIBS=%RTERM_LIBS% -DRTERM_SDL2_PATH=%DEPS_PATH% -G "Visual Studio 15 2017 Win64" &&^
+cmake .. -DRTERM_BUILD_TESTS=ON -DRTERM_EXTERNAL_LIBS=%RTERM_LIBS% -DRTERM_SDL2_PATH=%DEPS_PATH% -G "Visual Studio 15 2017" -A x64 &&^
 cmake --build . --config Release &&^
 popd ||^
 echo Build failed!
