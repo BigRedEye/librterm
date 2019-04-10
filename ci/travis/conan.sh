ffb72935f6edcf4f89b027575e5bc128518524db
BigRedEye <mail@bigredeye.me>
2019-04-10 15:47:44 +0300

    ci: pip3

--- ci/travis/conan.sh
+++ ci/travis/conan.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-pip install --user --upgrade pip
-pip install --user conan
+pip3 install --user --upgrade pip3
+pip3 install --user conan
 
-pip --version
+pip3 --version
 conan --version
 conan user
