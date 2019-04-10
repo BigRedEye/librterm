ccd847eeb025ef0037c2e0d08894d549adf66db2
BigRedEye <mail@bigredeye.me>
2019-04-11 02:25:02 +0300

    ci(appveyor): Fix

--- ci/appveyor/run/run.bat
+++ ci/appveyor/run/run.bat
@@ -1,6 +1,5 @@
-pushd %cd% &&^
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat" &&^
-dumpbin /DEPENDENTS build\bin\Release\benchmark.exe &&^
-build\bin\Release\benchmark.exe build\bin\resources 300 500 &&^
-popd ||^
-echo Run failed!
+pushd %cd%
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
+dumpbin /DEPENDENTS build\bin\Release\benchmark.exe
+build\bin\Release\benchmark.exe build\bin\resources 300 500
+popd
