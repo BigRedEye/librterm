16becd7d32a79cd21345db631979ec2e32f8f8ed
BigRedEye <mail@bigredeye.me>
2019-04-11 15:36:53 +0300

    ci(travis): Commit message

--- ci/travis/local.sh
+++ ci/travis/local.sh
@@ -3,6 +3,5 @@
 # Based on https://hg.libsdl.org/SDL/file/default/docs/README-linux.md
 sudo apt-get install libtool libasound2-dev libpulse-dev libaudio-dev libx11-dev libxext-dev \
     libxrandr-dev libxcursor-dev libxi-dev libxinerama-dev libxxf86vm-dev \
-    libxss-dev libgl1-mesa-dev libesd0-dev libdbus-1-dev libudev-dev \
-    libgles1-mesa-dev libgles2-mesa-dev libegl1-mesa-dev libibus-1.0-dev \
+    libxss-dev libgl1-mesa-dev libesd0-dev libdbus-1-dev libudev-dev libibus-1.0-dev \
     fcitx-libs-dev libsamplerate0-dev libsndio-dev libxkbcommon-dev
